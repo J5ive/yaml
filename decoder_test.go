c6ab1f92d282369f051556271db042d2803940b9
J5ive <j5ive@qq.com>
2014-03-26 00:15:57 +0800

    Create decoder_test.go

---
+++ decoder_test.go
@@ -0,0 +1,51 @@
+package yaml
+
+import (
+	"reflect"
+	"testing"
+)
+
+func assertEqual(t *testing.T, x, y interface{}) {
+	if !reflect.DeepEqual(x, y) {
+		t.Errorf("Assert fail: \nExpect: %v\nObtain: %v\n", x, y)
+	}
+}
+
+func  TestDecodeSimple(t *testing.T) {
+	data := []byte(`
+a: 1
+
+b : abc
+
+# comment
+c: >
+  abc
+  def
+
+D :
+
+E :
+  # comment
+  - 1
+  - 2
+  - 3 #comment
+
+
+`))
+
+	var s struct {
+		A int 		`yaml:"a"`
+		B string 	`yaml:"b"`
+		C string 	`yaml:"c"`
+		D string
+		E []int
+	}
+	
+	err := Unmarshal(data, &s)
+	assertEqual(err, nil)
+	assertEqual(t, s.A, 1)
+	assertEqual(t, s.B, "abc")
+	assertEqual(t, s.C, "abc def\n")
+	assertEqual(t, s.D, "")
+	assertEqual(t, s.E, []int{1,2,3})
+}
